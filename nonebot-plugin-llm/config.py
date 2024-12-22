39e7b2814c5b93f96011a39177516e6bbdd9cef0
noeru_desu <3203911711@qq.com>
2024-12-22 19:54:00 +0800

    去除多余的#

--- nonebot-plugin-llm/config.py
+++ nonebot-plugin-llm/config.py
@@ -90,7 +90,7 @@ class LLMConfig:
         for key, checker in self.config_checkers.items():
             if checker.comment is None:
                 continue
-            commented_map.yaml_set_comment_before_after_key(key, f'# {checker.comment}')
+            commented_map.yaml_set_comment_before_after_key(key, checker.comment)
         return commented_map
 
     def get_kv(self) -> Generator[Tuple[str, Any]]:
