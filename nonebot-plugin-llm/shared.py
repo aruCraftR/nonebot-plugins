20643e7a4695c87819ea2a92f2ee486f1c8fb74d
noeru_desu <3203911711@qq.com>
2025-01-13 18:43:32 +0800

    Update shared.py

--- nonebot-plugin-llm/shared.py
+++ nonebot-plugin-llm/shared.py
@@ -1,11 +1,11 @@
 from typing import TYPE_CHECKING
 
-from pymilvus import MilvusClient
 from nonebot import logger as nb_logger
 from nonebot.config import Config
 from tiktoken import get_encoding as tiktoken_encoding
 
 if TYPE_CHECKING:
+    from pymilvus import MilvusClient
     from . import config
     from loguru import Record
 
@@ -16,4 +16,4 @@ nonebot_config: Config
 plugin_config: 'config.PluginConfig'
 tiktoken = tiktoken_encoding('cl100k_base')
 logger = nb_logger.bind().patch(__patcher)
-db_client: MilvusClient
+db_client: 'MilvusClient'
