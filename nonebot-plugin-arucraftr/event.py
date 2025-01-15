d4c2d86635c48e07009a576d13fc05f4d6c829e7
noeru_desu <3203911711@qq.com>
2025-01-15 20:11:13 +0800

    防截胡

--- nonebot-plugin-arucraftr/event.py
+++ nonebot-plugin-arucraftr/event.py
@@ -18,9 +18,9 @@ admin_group_message = on_message(
     rule=rule_from_admin_group & rule_forbidden_id & rule_available_message
 )
 
-main_group_notice = on_notice(rule=rule_forbidden_id, permission=permission_main_group)
+main_group_notice = on_notice(rule=rule_forbidden_id, permission=permission_main_group, priority=5000, block=False)
 
-admin_group_notice = on_notice(permission=permission_admin_group)
+admin_group_notice = on_notice(permission=permission_admin_group, priority=5000, block=False)
 
 @main_group_message.handle()
 async def main_group_message_handler(event: MessageEvent, bot: Bot):
